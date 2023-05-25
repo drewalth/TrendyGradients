821384933f2904d55d19ba09e6804320a350f57b
Drew Althage <andrew.althage@gmail.com>
2023-05-25 10:24:35 -0600

    feat: animated background example

--- README.md
+++ README.md
@@ -1,53 +1,5 @@
 # TrendyGradients
 
-Super cool boxes with trendy neon gradient backgrounds for SwiftUI.
+Super cool boxes with trendy neon gradient backgrounds for SwiftUI. And animated background gradient example in a Swift Playground.
 
-![photo](trendy.png)
-
-```swift
-import SwiftUI
-
-struct ContentView: View {
-    let colors: [Color] = [
-        .red, .green, .blue, .orange, .yellow, .pink, .purple, .blue, .indigo, .mint
-    ]
-    
-    var body: some View {
-        ScrollView {
-            let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
-            
-            LazyVGrid(columns: columns) {
-                ForEach(0..<colors.count, id: \.self) { i in
-                    ForEach(i+1..<colors.count, id: \.self) { j in
-                        GradientView(colors: (colors[i], colors[j]))
-                            .padding()
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct GradientView: View {
-    let colors: (Color, Color)
-    
-    var body: some View {
-        Text("Hello")
-            .font(.title)
-            .foregroundColor(.white)
-            .frame(width: 100, height: 100, alignment: .center)
-            .background(
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(
-                        LinearGradient(
-                            gradient: Gradient(colors: [colors.0, colors.1]),
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    )
-                    .shadow(color: colors.0.opacity(0.5), radius: 6, x: 0.0, y: 0.0)
-            )
-    }
-}
-
-```
+![photo](trendy-hero.gif)
--- TrendyGradients.swiftpm/ContentView.swift
+++ TrendyGradients.swiftpm/ContentView.swift
@@ -8,7 +8,7 @@ struct ContentView: View {
     var body: some View {
         ScrollView {
             let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
-            
+            AnimatedExampleView()
             LazyVGrid(columns: columns) {
                 ForEach(0..<colors.count, id: \.self) { i in
                     ForEach(i+1..<colors.count, id: \.self) { j in
@@ -17,7 +17,7 @@ struct ContentView: View {
                     }
                 }
             }
-        }
+        }.padding(.top, 16)
     }
 }
 
@@ -40,5 +40,57 @@ struct GradientView: View {
                     )
                     .shadow(color: colors.0.opacity(0.5), radius: 6, x: 0.0, y: 0.0)
             )
+    }
+}
+
+struct AnimatedExampleView: View {
+    @State private var isAnimating = false
+
+    let colors1 = [Color.red, Color.blue]
+    let colors2 = [Color.green, Color.yellow]
+
+    var body: some View {
+        Text("Animated")
+            .font(.title)
+            .foregroundColor(.white)
+            .frame(width: 200, height: 200)
+            .background(
+                AnimatedGradientView(isAnimating: $isAnimating, colors1: colors1, colors2: colors2)
+            )
+            .onAppear {
+                withAnimation(Animation.linear(duration: 3).repeatForever(autoreverses: true)) {
+                    isAnimating = true
+                }
+            }
+    }
+}
+
+struct AnimatedGradientView: View {
+    @Binding var isAnimating: Bool
+    let colors1: [Color]
+    let colors2: [Color]
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: colors1),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .opacity(isAnimating ? 0 : 1)
+
+            RoundedRectangle(cornerRadius: 25)
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: colors2),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .opacity(isAnimating ? 1 : 0)
+        }
     }
 }
